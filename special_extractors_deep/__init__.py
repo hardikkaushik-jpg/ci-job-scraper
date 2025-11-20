4fc274bbb0a747028770a6115e09a852b0bf9eea
hardikkaushik-jpg <hardik.kaushik@hcl-software.com>
2025-11-20 14:16:12 +0530

    Update __init__.py

--- special_extractors_deep/__init__.py
+++ special_extractors_deep/__init__.py
@@ -1,35 +1,33 @@
 # __init__.py
-# Master registry for all 27 deep extractors
+# Master registry for all deep extractors
 
-from .extractor_alation import extract_alation
-from .extractor_amazon import extract_amazon
-from .extractor_ataccama import extract_ataccama
-from .extractor_atlan import extract_atlan
-from .extractor_bigeye import extract_bigeye
-from .extractor_cloudera import extract_cloudera
-from .extractor_collibra import extract_collibra
-from .extractor_couchbase import extract_couchbase
-from .extractor_dataworld import extract_dataworld
-from .extractor_datadog import extract_datadog
-from .extractor_decube import extract_decube
-from .extractor_exasol import extract_exasol
-from .extractor_firebolt import extract_firebolt
-from .extractor_ibm import extract_ibm
-from .extractor_informatica import extract_informatica
-from .extractor_influxdata import extract_influxdata
-from .extractor_matillion import extract_matillion
-from .extractor_mongodb import extract_mongodb
-from .extractor_oracle import extract_oracle
-from .extractor_pentaho import extract_pentaho
-from .extractor_qlik import extract_qlik
-from .extractor_sifflet import extract_sifflet
-from .extractor_solidatus import extract_solidatus
-from .extractor_syniti import extract_syniti
-from .extractor_teradata import extract_teradata
-from .extractor_vertica import extract_vertica
-from .extractor_yellowbrick import extract_yellowbrick
-from .extractor_sap import extract_sap
-from .extractor_salesforce import extract_salesforce
+from .alation import extract_alation
+from .amazon import extract_amazon
+from .ataccama import extract_ataccama
+from .atlan import extract_atlan
+from .bigeye import extract_bigeye
+from .cloudera import extract_cloudera
+from .collibra import extract_collibra
+from .couchbase import extract_couchbase
+from .dataworld import extract_dataworld
+from .datadog import extract_datadog
+from .decube import extract_decube
+from .exasol import extract_exasol
+from .firebolt import extract_firebolt
+from .ibm import extract_ibm
+from .informatica import extract_informatica
+from .influxdata import extract_influxdata
+from .oracle import extract_oracle
+from .pentaho import extract_pentaho
+from .qlik import extract_qlik    
+from .sifflet import extract_sifflet
+from .solidatus import extract_solidatus
+from .syniti import extract_syniti
+from .teradata import extract_teradata
+from .vertica import extract_vertica
+from .yellowbrick import extract_yellowbrick
+from .sap import extract_sap
+from .salesforce import extract_salesforce
 
 # Master registry
 SPECIAL_EXTRACTORS_DEEP = {
@@ -49,8 +47,6 @@ SPECIAL_EXTRACTORS_DEEP = {
     "IBM": extract_ibm,
     "Informatica": extract_informatica,
     "InfluxData": extract_influxdata,
-    "Matillion": extract_matillion,
-    "MongoDB": extract_mongodb,
     "Oracle": extract_oracle,
     "Pentaho": extract_pentaho,
     "Qlik": extract_qlik,
@@ -63,4 +59,3 @@ SPECIAL_EXTRACTORS_DEEP = {
     "SAP": extract_sap,
     "Salesforce": extract_salesforce,
 }
-
